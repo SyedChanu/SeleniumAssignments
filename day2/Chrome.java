fe515f990cf7f20609d35107e32e255d37bc7afe
SyedChanu <syedchanu56@gmail.com>
2024-09-11 00:25:44 +0530

    Add files via upload
    Week1Day2

---
+++ day2/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortCutKey='C';
+		
+		System.out.println("Version:"+version+" Developer:"+developer+" Beta:"+isBeta+" ReleaseYear:"+releaseYear+" ShortCutKey:"+ shortCutKey);
+
+	}
+
+}
---
+++ day2/DataTypesPractise.java
@@ -0,0 +1,35 @@
+package week1.day2;
+
+public class DataTypesPractise {
+
+	public static void main(String[] args) {
+		//Datatypes --->Primitive and non primitive
+		//Primitive types-Integer,Float,Character,Boolean(Wrapper Classes)
+		//Integer types
+		byte b=12;
+		short s=200;
+		int i=7777;
+		long l=8940936973L;
+		//Float types
+		float f=13.734F;
+		double d=156.7865477;
+		//Character
+		char c='F';
+		//prints ascii code if you give integers in char
+		char c1=77;
+		//Boolean--> True or False
+		boolean understood=true;
+		
+		//Non Primitive--String and Arrays
+		//String
+		String command="I am ahead of the league";
+		
+		System.out.println("Integer types: Byte:"+b+"  Short:"+s+" Int:"+i+" Long:"+l);
+		System.out.println("Float types: Float:"+f+"  Double:"+d);
+		System.out.println("Char:"+c+" Ascii value for char77 is:"+c1);
+		System.out.println("Boolean value:"+understood);
+		
+
+	}
+
+}
